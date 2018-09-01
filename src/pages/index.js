6fdf930f009ca4dd5b6d5c9655510e72791b5f9b
Ali Baba <alibabatorjan001@gmail.com>
2018-09-01 23:46:10 +0500

    added the methods for pushing the code online

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,14 @@ const IndexPage = () => (
     <p>Hi This is me , <b>Ali</b> .Its my first time for being acting as a candidate for a HAckathon in U.S.</p>
     <p>I also applied to the JAMstack hello world hackathon</p>
     <p>I am also a university student </p>
+
+<h2>How to Push changes in my code to be visible at domain</h2>
+<ol>
+<li>Commit stage or unstage changes via VS code</li>
+<li>git push origin master</li>
+<li><code>gatsby build</code>on terminal</li>
+
+</ol>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
