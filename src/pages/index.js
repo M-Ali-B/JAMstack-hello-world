57c9609679df66ab13b8a4114ea055bd230d86ec
root <root@Phenomenal.localdomain>
2018-09-01 22:17:09 +0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Hi This is me , <b>Ali</b> .Its my first time for being acting as a candidate for a HAckathon in U.S.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
